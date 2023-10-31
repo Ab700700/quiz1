b3c4ad901013f28a2a1bf9ded7b6db3ae973b558
Ab700700 <148686321+Ab700700@users.noreply.github.com>
2023-10-31 11:06:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            // ---- 1 ----
+
+//        System.out.println("Enter a number: ");
+//        int a = scan.nextInt();
+//
+//        if(a%2==0) System.out.println("the number is even");
+//        else System.out.println("the number is odd");
+
+
+        // ---- 2 ----
+
+//            int sum = 0;
+//            String s;
+//        do{
+//            System.out.println("Enter number :");
+//            int num = scan.nextInt();
+//            sum+= num;
+//
+//            System.out.println("Enter stop to end or anything to continue");
+//            s=scan.next();
+//
+//        }while(!s.equals("stop"));
+//
+//        System.out.println("the sum is "+sum);
+
+        // ---- 3 ----
+
+//        int ar []  = {10,-21,30,31,-25};
+//        for(int i = 0; i < ar.length; i++) {
+//            if(ar[i]>0) System.out.printf("%d is a positive number\n",ar[i]);
+//            else if(ar[i]<0) System.out.printf("%d is a negative number\n",ar[i]);
+//            else continue;
+//        }
+
+        // ---- 4 ----
+
+//        String ar [] = {"Tuwaiq", "Bootcamp", "Student","JAVA"};
+//        String shortestWord = ar[0];
+//
+//        for(int i = 1 ; i<ar.length;i++){
+//            if(ar[i].length()<shortestWord.length()) shortestWord= ar[i];
+//        }
+//        System.out.println(shortestWord);
+
+    }
+}
